f22fb7c6a982818fa408f08d490f1f595ace0a66
Ton0703 <44728377+Ton0703@users.noreply.github.com>
2020-05-31 23:43:50 +0800

    Updates

---
+++ precache-manifest.4eb414816acd41eb9e7f8704452ac4f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd92999df10337b42da86d004661b9e2",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "11a8dd4c9a611003e71d",
+    "url": "/weather-app/static/css/main.51b9c0da.chunk.css"
+  },
+  {
+    "revision": "7754d9e896ffec05a176",
+    "url": "/weather-app/static/js/2.57ea04e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.57ea04e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11a8dd4c9a611003e71d",
+    "url": "/weather-app/static/js/main.fdb37daa.chunk.js"
+  },
+  {
+    "revision": "82c9b66290339700053d",
+    "url": "/weather-app/static/js/runtime-main.6c7a7c51.js"
+  }
+]);
